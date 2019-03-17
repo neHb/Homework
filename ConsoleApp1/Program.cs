5042567a2584cd75de34aba3464a27e13382788a
Александр Бакунин <sasha-bakunin@outlook.com>
2019-03-17 22:56:13 +0500

    cc

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -14,17 +14,29 @@ namespace ConsoleApp1
         public double x0 = 0;
         public double a = 0;
         public double U0 = 0;
-        public double dt = 0;  
+        public double dt = 0;
+        public double m = 0;
+        public double k = 0;
         public void Motion_Path(ref List<Coordinates> coordinates)
         {
-            
-            double tp = 2 * U0 * Math.Sin(a) / 9.8; 
+            double xpos_p = 0;
+            double ypos_p = 0;
+            double Ux_p = U0 * Math.Cos(a * Math.PI / 180);
+            double Uy_p = U0 * Math.Sin(a * Math.PI / 180);
+            double Ux = 0;
+            double Uy = 0;
             a = a * 3.14 / 180;
-            for (double t = 0; t <= tp; t += dt)
+            while (ypos_p >= 0)
             {
                 Coordinates NewCoor = new Coordinates();
-                NewCoor.xpos = x0 + U0 * Math.Cos(a) * t;
-                NewCoor.ypos = U0 * Math.Sin(a) * t - (9.8 * t * t / 2);
+                NewCoor.xpos = xpos_p + Ux_p * dt;
+                NewCoor.ypos = ypos_p + Uy_p * dt;
+                Ux = Ux_p * (1 - k / m * dt);
+                Uy = Uy_p - dt * (9.8 + k / m * Uy_p);
+                xpos_p = NewCoor.xpos;
+                ypos_p = NewCoor.ypos;
+                Ux_p = Ux;
+                Uy_p = Uy;
                 coordinates.Add(NewCoor);
             }
         }
@@ -42,6 +54,8 @@ namespace ConsoleApp1
             Motion.a = Convert.ToDouble(sr.ReadLine());
             Motion.U0 = Convert.ToDouble(sr.ReadLine());
             Motion.dt = Convert.ToDouble(sr.ReadLine());
+            Motion.k = Convert.ToDouble(sr.ReadLine());
+            Motion.m = Convert.ToDouble(sr.ReadLine());
             sr.Close();
             Motion.Motion_Path(ref coordinates);
             StreamWriter sw = new StreamWriter(link2);
