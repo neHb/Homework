6c8f26050021330ecbd702fa9391e3a82b47aebe
Александр Бакунин <sasha-bakunin@outlook.com>
2019-03-18 00:31:50 +0500

    отредактировано

--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -3,41 +3,103 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
-
-namespace Petzold.ScrollFiftyButtons
+namespace Petzold.CalculateYourLife
 {
-    class ScrollFiftyButtons : Window
+    class CalculateYourLife : Window
     {
-        [STAThread]
-        public static void Main()
+        TextBox txtboxBegin, txtboxEnd;//создание текстовых областей для ввода 
+        Label lblLifeYears;
+        [STAThread] public static void Main()
         {
             Application app = new Application();
-            app.Run(new ScrollFiftyButtons());
-        }
-        public ScrollFiftyButtons()
+            app.Run(new CalculateYourLife()); }
+        public CalculateYourLife()
         {
-            Title = "Scroll Fifty Buttons";
-            SizeToContent = SizeToContent.Width;
-            AddHandler(Button.ClickEvent, new RoutedEventHandler(ButtonOnClick));
-            ScrollViewer scroll = new ScrollViewer();
-            Content = scroll;
-            StackPanel stack = new StackPanel();
-            stack.Margin = new Thickness(10);
-            scroll.Content = stack;
-            for (int i = 0; i < 50; i++)
+            Title = "Calculate Your Life";//объявляет и присваивает заголовок 
+            SizeToContent = SizeToContent.WidthAndHeight;//изменение ширины и длины окна по содержимому
+            ResizeMode = ResizeMode.CanMinimize;//изменение размера окна к минимуму 
+            Grid grid = new Grid();//задание области с таблицей из строк и столбцов 
+            Content = grid;
+            for (int i = 0; i < 3; i++)
             {
-                Button btn = new Button();
-                btn.Name = "Button" + (i + 1);
-                btn.Content = btn.Name + " says  'Click me'";
-                btn.Margin = new Thickness(10);
-                stack.Children.Add(btn);
+                RowDefinition rowdef = new  RowDefinition();// определение свойств для строки
+                rowdef.Height = GridLength.Auto;
+                grid.RowDefinitions.Add(rowdef);
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                ColumnDefinition coldef = new  ColumnDefinition();// определение свойств для столбца
+                coldef.Width = GridLength.Auto;
+                grid.ColumnDefinitions.Add(coldef);
+            }
+            Label lbl = new Label();// создание надписи
+            lbl.Content = "Begin Date: ";
+            grid.Children.Add(lbl);
+            Grid.SetRow(lbl, 0);
+            Grid.SetColumn(lbl, 0);
+            txtboxBegin = new TextBox();
+            txtboxBegin.Text = new DateTime(1980,  1, 1).ToShortDateString();
+            txtboxBegin.TextChanged += TextBoxOnTextChanged;
+            grid.Children.Add(txtboxBegin);
+            Grid.SetRow(txtboxBegin, 0);
+            Grid.SetColumn(txtboxBegin, 1);
+            lbl = new Label();
+            lbl.Content = "End Date: ";
+            grid.Children.Add(lbl);
+            Grid.SetRow(lbl, 1);
+            Grid.SetColumn(lbl, 0);
+            txtboxEnd = new TextBox();
+            txtboxEnd.TextChanged +=  TextBoxOnTextChanged;
+            grid.Children.Add(txtboxEnd);
+            Grid.SetRow(txtboxEnd, 1);
+            Grid.SetColumn(txtboxEnd, 1);
+            lbl = new Label();
+            lbl.Content = "Life Years: ";
+            grid.Children.Add(lbl);
+            Grid.SetRow(lbl, 2);
+            Grid.SetColumn(lbl, 0);
+            lblLifeYears = new Label();
+            grid.Children.Add(lblLifeYears);
+            Grid.SetRow(lblLifeYears, 2);
+            Grid.SetColumn(lblLifeYears, 1);
+            Thickness thick = new Thickness(5);
+            grid.Margin = thick;
+            foreach (Control ctrl in grid.Children)
+                ctrl.Margin = thick;
+            txtboxBegin.Focus();
+            txtboxEnd.Text = DateTime.Now .ToShortDateString();
+        }
+        void TextBoxOnTextChanged(object sender,  TextChangedEventArgs args)//расчет разности между датами 
+        {
+            DateTime dtBeg, dtEnd;
+            if (DateTime.TryParse(txtboxBegin.Text , out dtBeg) &&      
+                DateTime.TryParse(txtboxEnd.Text,  out dtEnd))
+            {
+                int iYears = dtEnd.Year - dtBeg.Year;
+                int iMonths = dtEnd.Month - dtBeg .Month;
+                int iDays = dtEnd.Day - dtBeg.Day;
+                if (iDays < 0)
+                {
+                    iDays += DateTime.DaysInMonth (dtEnd.Year,     
+                        1 +  (dtEnd.Month + 10) % 12);
+                    iMonths -= 1;
+                }
+                if (iMonths < 0)
+                {
+                    iMonths += 12;
+                    iYears -= 1;
+                }
+                lblLifeYears.Content =       
+                    String.Format("{0} year{1}, {2 } month{3}, {4} day{5}",     
+                    iYears, iYears  == 1 ? "" : "s",        
+                    iMonths, iMonths  == 1 ? "" : "s",     
+                    iDays, iDays ==  1 ? "" : "s");
+            }
+            else
+            {
+                lblLifeYears.Content = "";
             }
         }
-        void ButtonOnClick(object sender, RoutedEventArgs args)
-        {
-            Button btn = args.Source as Button;
-            if (btn != null)
-                MessageBox.Show(btn.Name + " has  been clicked", "Button Click");
-        }
     }
-}
+} 
+
