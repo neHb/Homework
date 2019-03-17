29e08942169ef62b448992a1f55a5c4a69b2dc5f
Александр Бакунин <sasha-bakunin@outlook.com>
2019-03-18 00:01:04 +0500

    след 2 задание

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -15,15 +15,15 @@ namespace Petzold.ScrollFiftyButtons
         }
         public ScrollFiftyButtons()
         {
-            Title = "Scroll Fifty Buttons";
-            SizeToContent = SizeToContent.Width;
-            AddHandler(Button.ClickEvent, new RoutedEventHandler(ButtonOnClick));
-            ScrollViewer scroll = new ScrollViewer();
-            Content = scroll;
-            StackPanel stack = new StackPanel();
-            stack.Margin = new Thickness(5);
+            Title = "Scroll Fifty Buttons"; //объявляет и присваивает заголовок 
+            SizeToContent = SizeToContent.Width; //изменение ширины окна по содержимому
+            AddHandler(Button.ClickEvent, new RoutedEventHandler(ButtonOnClick)); //проверка 
+            ScrollViewer scroll = new ScrollViewer();//объявление прокрутки в недоступные области 
+            Content = scroll; //добавить прокрутку 
+            StackPanel stack = new StackPanel(); //выравнивание дочерних элементов в одну линию
+            stack.Margin = new Thickness(5);// расстояние между объектами 
             scroll.Content = stack; 
-            for (int i = 0; i < 50; i++) 
+            for (int i = 0; i < 50; i++) //создание 50 кнопок с надписью Button 'номер' says 'Click me'
             { 
                 Button btn = new Button(); 
                 btn.Name = "Button" + (i + 1); 
@@ -32,7 +32,7 @@ namespace Petzold.ScrollFiftyButtons
                 stack.Children.Add(btn); 
             } 
         } 
-        void ButtonOnClick(object sender, RoutedEventArgs args) 
+        void ButtonOnClick(object sender, RoutedEventArgs args) //вызов события при нажатии кнопки 
         { 
         Button btn = args.Source as Button; 
         if (btn != null) 
